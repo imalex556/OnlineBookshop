cd838da22e3df390fe472b753b32e8eb9de2ddc1
Alex <148885151+imalex556@users.noreply.github.com>
2025-04-23 18:16:44 +0100

    Added User entity

---
+++ OnlineBookshop/src/main/java/com/example/OnlineBookshop/model/User.java
@@ -0,0 +1,30 @@
+package com.example.OnlineBookshop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    
+    @Column(unique = true, nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+    
+    public enum Role {
+        CUSTOMER, ADMIN
+    }
+}
